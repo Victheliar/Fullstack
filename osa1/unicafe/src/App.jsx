7c2047ecdda22cb81cdf5074f4963962fd97bb4b
Victoria Khoreva <victheliar@gmail.com>
2026-02-11 13:48:47 +0200

    exercise 1.8 completed

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad
+  return(
+  <div>
+    <h2>statistics</h2>
+    <p>good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+    <p>all {all}</p>
+    <p>average {(props.good - props.bad)/all}</p>
+    <p>positive {(props.good/all)*100} %</p>
+  </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -12,13 +27,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad)/(good + neutral + bad)}</p>
-      <p>positive {good/(good + neutral + bad)} %</p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
