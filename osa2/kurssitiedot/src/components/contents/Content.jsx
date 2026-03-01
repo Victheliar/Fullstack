b7d0e6196994edae7e82b35622d38219000b57a5
Victoria Khoreva <victheliar@gmail.com>
2026-03-01 14:16:15 +0200

    exercise 2.2 completed

--- osa2/kurssitiedot/src/components/contents/Content.jsx
+++ osa2/kurssitiedot/src/components/contents/Content.jsx
@@ -1,12 +1,13 @@
 import Part from './Part'
 
 const Content = ({content}) => {
-    console.log({content})
+    // console.log({content})
     return (
         <div>
             <Part part={content[0]} />
             <Part part={content[1]} />
             <Part part={content[2]} />
+            <b>total of {content[0].exercises + content[1].exercises + content[2].exercises} exercises</b>
         </div>
     )
 }
