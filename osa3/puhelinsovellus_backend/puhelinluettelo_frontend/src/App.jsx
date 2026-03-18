79e75b81024e28a47c50ce82d072b96fcdf60d3a
Victoria Khoreva <victheliar@gmail.com>
2026-03-18 19:10:35 +0200

    exercise 3.9 completed

---
+++ osa3/puhelinsovellus_backend/puhelinluettelo_frontend/src/App.jsx
@@ -0,0 +1,175 @@
+import { useState, useEffect } from 'react'
+import contactService from './services/contacts'
+import Notification from './components/Notification'
+import Error from './components/Error'
+
+const Filter = ({filter}) => {
+  return (
+      <form>
+        <div>
+          filter shown with <input onChange={filter}/>
+        </div>
+      </form>
+  )
+}
+
+const PersonForm = ({person, name, number}) => {
+  return (
+      <form onSubmit={person}>
+        <div>
+          name: <input onChange={name} />
+        </div>
+        <div>
+          number: <input onChange={number} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  )
+}
+
+const Persons = ({numbers, remove}) => {
+  return (
+      <ul>
+          {numbers.map((person) => (
+            <li key={person.id}>{person.name} {person.number}<button onClick={() => remove(person.id)}>delete</button></li>
+          ))}
+      </ul>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  const [notification, setNotification] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const showNotification = (message) => {
+    setNotification(message)
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
+  }
+
+  useEffect(() => {
+    contactService
+      .getAll()
+      .then(initialContacts => {
+        setPersons(initialContacts)
+      })
+  }, [])
+
+  const handleNewPerson = (event) => {
+    event.preventDefault()
+    const nameObject = {name: newName, number: newNumber}
+    const namePresent = persons.find(person => newName.toLowerCase() === person.name.toLowerCase());
+    if (namePresent && window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
+        contactService
+        .update(namePresent.id, nameObject)
+        .then(returnedContact => {
+          setPersons(persons.map(person => person.id !== returnedContact.id ? person : returnedContact))
+          setNewName('')
+          setNewNumber('')
+        })
+        .then((notification) => {
+          setNotification(
+            `Updated ${namePresent.name}`
+          )
+          setTimeout(() => {
+            setNotification(null)
+          }, 5000)
+        })
+        .catch((error) => {
+          setErrorMessage(
+            `Information of ${namePresent.name} has already been removed from the server`
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+        })
+    } else {
+    contactService
+      .create(nameObject)
+      .then(returnedContact => {
+        setPersons(persons.concat(returnedContact))
+        setNewName('')
+        setNewNumber('')
+      })
+      .then((notification) => {
+        setNotification(
+          `Added ${newName}`
+        )
+        setTimeout(() => {
+          setNotification(null)
+        }, 5000)
+      })
+    }
+  }
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleNewFilter = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const personsToShow = persons.filter((person) => {
+    return person.name.toLowerCase().includes(newFilter.toLowerCase())
+  })
+
+  const removeContact = (id) => {
+    const person = persons.find(c => c.id === id)
+    const updatedContacts = persons.filter(person => person.id !== id)
+
+    if (window.confirm(`Delete ${person.name}?`)){
+      contactService
+        .remove(person.id)
+        .then(setPersons(updatedContacts))
+        .then(notification => {
+          setNotification(
+           `Deleted ${person.name}` 
+          )
+          setTimeout(() => {
+            setNotification(null)
+          }, 5000)
+        })
+        .catch(error => {
+          setErrorMessage(
+            `${person.name} was already deleted`
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+        })
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Notification message={notification} />
+      <Error message={errorMessage} />
+
+      <Filter filter={handleNewFilter}/>
+
+      <h2>add a new</h2>
+
+      <PersonForm person={handleNewPerson} name={handleNewName} number={handleNewNumber} />
+
+      <h2>Numbers</h2>
+
+      <Persons numbers={personsToShow} remove={removeContact} />
+    </div>
+  )
+}
+
+export default App
