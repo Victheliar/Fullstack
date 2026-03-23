359a53118eeb79afb2ff1dadcfaa81ce6afa556f
Victoria Khoreva <victheliar@gmail.com>
2026-03-23 18:30:56 +0200

    exercise 3.15 completed

--- osa3/puhelinsovellus_backend/backend/index.js
+++ osa3/puhelinsovellus_backend/backend/index.js
@@ -40,10 +40,12 @@ app.get("/api/persons/:id", (request, response) => {
     }
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete("/api/persons/:id", (request, response, next) => {
+    Persons.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post("/api/persons", (request, response) => {
