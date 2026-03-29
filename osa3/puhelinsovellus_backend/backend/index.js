34c9fb1734184a835afb7ffcc8f76fd017986160
Victoria Khoreva <victheliar@gmail.com>
2026-03-29 16:17:00 +0300

    exercise 3.19 started?

--- osa3/puhelinsovellus_backend/backend/index.js
+++ osa3/puhelinsovellus_backend/backend/index.js
@@ -23,6 +23,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
@@ -84,8 +86,9 @@ app.delete("/api/persons/:id", (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body
+    
     if (!body.name) {
         return response.status(400).json({
             error: 'name missing'
@@ -105,6 +108,7 @@ app.post("/api/persons", (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
