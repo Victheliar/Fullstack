d91edf7908703f21c3f6f39f60d27b7e1312863b
Victheliar <victheliar@gmail.com>
2026-04-24 18:11:25 +0300

    start exercise 4.1

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUrl = 'mongodb://localhost/bloglist';
+mongoose.connect(mongoUrl, { family: 4 })
+
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs);
+    });
+});
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body);
+
+    blog.save().then((result) => {
+        response.status(201).json(result);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
