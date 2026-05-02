ed8734fa6c2cd639a835e807e20f50aef5a0d357
Victheliar <victheliar@gmail.com>
2026-05-02 20:38:39 +0300

    exercise 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,4 +6,11 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
-module.exports = { dummy, totalLikes}
+const favoriteBlog = (blogs) => {
+    const reducer = (favorite, blog) => {
+        return favorite.likes > blog.likes ? favorite : blog
+    }
+    return blogs.length === 0 ? {} : blogs.reduce(reducer, 0)
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
