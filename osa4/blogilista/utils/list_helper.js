966d1906c7dcb2bc14a88be436aca82082802add
Victheliar <victheliar@gmail.com>
2026-05-03 14:21:58 +0300

    exercise 4.6 completed

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -13,4 +15,17 @@ const favoriteBlog = (blogs) => {
     return blogs.length === 0 ? {} : blogs.reduce(reducer, 0)
 }
 
-module.exports = { dummy, totalLikes, favoriteBlog }
+const mostBlogs = (blogs) => {
+    const blogsByAuthor = _.countBy(blogs, 'author')
+    const reducer = (most, author) => {
+        return most.blogs > blogsByAuthor[author] ? most : { author : author, blogs : blogsByAuthor[author] }
+    }
+    return blogs.length === 0 ? {} : Object.keys(blogsByAuthor).reduce(reducer, 0)
+}
+
+module.exports = { 
+    dummy, 
+    totalLikes, 
+    favoriteBlog,
+    mostBlogs
+}
