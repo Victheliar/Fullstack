7691c2a1c11cf96a01ea348a9abddf4d116cfab3
Victheliar <victheliar@gmail.com>
2026-05-04 19:49:41 +0300

    exercise 4.7 completed

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -23,9 +23,19 @@ const mostBlogs = (blogs) => {
     return blogs.length === 0 ? {} : Object.keys(blogsByAuthor).reduce(reducer, 0)
 }
 
+const mostLikes = (blogs) => {
+    const blogsByAuthor = _.groupBy(blogs, 'author')
+    const reducer = (most, author) => {
+        const authorLikes = blogsByAuthor[author].reduce((sum, blog) => sum + blog.likes, 0)
+        return most.likes > authorLikes ? most : { author : author, likes : authorLikes }
+    }
+    return blogs.length === 0 ? {} : Object.keys(blogsByAuthor).reduce(reducer, 0)
+}
+
 module.exports = { 
     dummy, 
     totalLikes, 
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
