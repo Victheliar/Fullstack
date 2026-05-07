507e25adc34d0e976dc14179f52b34003e02c695
Victheliar <victheliar@gmail.com>
2026-05-07 13:02:17 +0300

    exercise 4.10 completed

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -18,7 +18,7 @@ blogsRouter.get('/', async (request, response) => {
 //         .catch((error) => next(error))
 // })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
     const blog = new Blog({
@@ -28,12 +28,8 @@ blogsRouter.post('/', (request, response, next) => {
         likes: body.likes || 0
     })
 
-    blog
-        .save()
-        .then((savedBlog) => {
-            response.status(201).json(savedBlog)
-        })
-        .catch((error) => next(error))
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
 })
 
 blogsRouter.put('/:id', (request, response, next) => {
--- osa4/blogilista/tests/blogs_api.test.js
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -3,34 +3,18 @@ const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper')
 const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-const initialBlogs = [
-    {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-        __v: 0
-    },
-    {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0
-    }
-]
-
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
+
+    let blogObject = new Blog(helper.initialBlogs[0])
     await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
+
+    blogObject = new Blog(helper.initialBlogs[1])
     await blogObject.save()
 })
 
@@ -41,12 +25,31 @@ test('correct amount of blogs are returned as json', async () => {
         .expect('Content-Type', /application\/json/)
         const response = await api.get('/api/blogs')
         
-    assert.strictEqual(response.body.length, initialBlogs.length)
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
 test('blog id is defined as id', async () => {
     const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body[0].id, initialBlogs[0]._id)
+    assert.strictEqual(response.body[0].id, helper.initialBlogs[0]._id)
+})
+
+test('a valid blog can be added', async() => {
+    const newBlog = {
+        title: "Test Blog",
+        author: "Test Author",
+        url: "http://www.testblog.com",
+        likes: 10
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+    assert.strictEqual(blogsAtEnd[blogsAtEnd.length - 1].title, newBlog.title)
 })
 
 after(async () => {
