c501a3ab2546c28db3a4c18046ab6eb166e57dd8
Victheliar <victheliar@gmail.com>
2026-05-17 15:00:01 +0300

    exercise 4.15 completed

--- osa4/blogilista/models/user.js
+++ osa4/blogilista/models/user.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    username: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true // username oltava yksikäsitteinen
+    },
     name: String,
     passwordHash: String,
     blogs: [
--- osa4/blogilista/tests/users_api.test.js
+++ osa4/blogilista/tests/users_api.test.js
@@ -21,5 +21,44 @@ describe('when there is initially one user at db', () => {
 
     test('creation succeeds with a fresh username', async () => {
         const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'vici',
+            name: 'Victoria Khoreva',
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        
+        const usersAtEnd = await helper.usersInDb()
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        assert(usernames.includes(newUser.username))
+    })
+
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'root',
+            name: 'Superuser',
+            password: 'salainen',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+        
+        const usersAtEnd = await helper.usersInDb()
+        assert(result.body.error.includes('expected `username` to be unique'))
+
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length)
     })
 })
