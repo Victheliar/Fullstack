64a12a074704475a3a4d6b18390a5e239e7f027d
Victheliar <victheliar@gmail.com>
2026-08-01 17:40:06 +0300

    deleted the old attempt

--- osa5/blogilista-frontend/backend/app.js
+++
@@ -1,9 +0,0 @@
-require('express-async-errors')
-const express = require('express')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
-const loginRouter = require('./controllers/login')
-const usersRouter = require('./controllers/users')
-
-
-const app = express()
