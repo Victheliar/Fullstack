30481e67fb821048f988d75eb2b0db22c2d1cd28
Victheliar <victheliar@gmail.com>
2026-09-10 16:03:41 +0300

    start working on exercise 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
+test('renders title', () => {
     const blog = {
         title: 'Component testing is done with react-testing-library',
         author: 'Test Author'
@@ -11,4 +12,38 @@ test('renders content', () => {
 
     const element = screen.getByText('Component testing is done with react-testing-library Test Author')
     expect(element).toBeDefined()
+})
+
+test('renders url, likes and user when view button is clicked', async () => {
+
+    window.localStorage.setItem(
+        'loggedBlogappUser',
+        JSON.stringify({ username: 'testuser', name: 'Test User' })
+    )
+
+    const blog = {
+        title: 'Component testing is done with react-testing-library',
+        author: 'Vici',
+        url: 'https://yippeee.com',
+        likes: 5,
+        user: {
+            username: 'vici',
+            name: 'Viciii'
+        }
+    }
+
+    render(
+        <Blog blog={blog} />
+    )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const urlElement = screen.getByText('https://yippeee.com')
+    const likesElement = screen.getByText('likes 5')
+    const userElement = screen.getByText('vici')
+    expect(urlElement).toBeDefined()
+    expect(likesElement).toBeDefined()
+    expect(userElement).toBeDefined()
 })
